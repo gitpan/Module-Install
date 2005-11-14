7cf29079ec7621beb8ebc57f5829577792e2eaf8
☺唐鳳☻ <cpan@audreyt.org>
2005-11-14 18:08:00 +0000

    Import of AUTRIJUS/Module-Install-0.39 from CPAN.
    gitpan-cpan-distribution: Module-Install
    gitpan-cpan-version:      0.39
    gitpan-cpan-path:         AUTRIJUS/Module-Install-0.39.tar.gz
    gitpan-cpan-author:       AUTRIJUS
    gitpan-cpan-maturity:     released

--- inc/Module/Install.pm
+++ inc/Module/Install.pm
@@ -1,6 +1,6 @@
 #line 1 "inc/Module/Install.pm - lib/Module/Install.pm"
 package Module::Install;
-$VERSION = '0.38';
+$VERSION = '0.39';
 
 die << "." unless $INC{join('/', inc => split(/::/, __PACKAGE__)).'.pm'};
 Please invoke ${\__PACKAGE__} with:
